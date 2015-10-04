6a21079144faaffb9295e521cfe8468204faa0e7
Rikki Gibson <rikkigibson@gmail.com>
2015-10-03 17:26:49 -0700

    Web API 2 project now always returns JSON

--- CorvallisBusAPI-ASP4/Global.asax.cs
+++ CorvallisBusAPI-ASP4/Global.asax.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
@@ -12,6 +15,32 @@ namespace CorvallisBusAPI
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var jsonFormatter = new JsonMediaTypeFormatter();
+            GlobalConfiguration.Configuration.Services.Replace(typeof(IContentNegotiator), new JsonContentNegotiator(jsonFormatter));
+        }
+    }
+
+    /// <summary>
+    /// Shamelessly c/ped from stackoverflow.
+    /// </summary>
+    public class JsonContentNegotiator : IContentNegotiator
+    {
+        private readonly JsonMediaTypeFormatter _jsonFormatter;
+
+        public JsonContentNegotiator(JsonMediaTypeFormatter formatter)
+        {
+            _jsonFormatter = formatter;
+        }
+
+        public ContentNegotiationResult Negotiate(
+                Type type,
+                HttpRequestMessage request,
+                IEnumerable<MediaTypeFormatter> formatters)
+        {
+            return new ContentNegotiationResult(
+                _jsonFormatter,
+                new MediaTypeHeaderValue("application/json"));
         }
     }
 }
