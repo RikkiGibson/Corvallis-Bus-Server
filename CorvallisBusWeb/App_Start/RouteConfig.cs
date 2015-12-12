a890d005765b3c2d3ea6fc254eab6d942458fe7c
Rikki Gibson <rikkigibson@gmail.com>
2015-12-12 11:56:19 -0800

    Update RouteConfig to serve index.html with redirect to https

--- CorvallisBusWeb/App_Start/RouteConfig.cs
+++ CorvallisBusWeb/App_Start/RouteConfig.cs
@@ -14,12 +14,6 @@ namespace CorvallisBusWeb
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapMvcAttributeRoutes();
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
         }
     }
 }
