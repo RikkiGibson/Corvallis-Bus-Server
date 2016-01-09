aee0e4544ccc4b2ab449de8157e8ed8664e75f05
Rikki Gibson <rikkigibson@gmail.com>
2016-01-09 11:22:41 -0800

    Deliver Handoff app association file

---
+++ CorvallisBusWeb/Controllers/ExtensionlessFileDownloadController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace CorvallisBusWeb.Controllers
+{
+    /// <summary>
+    /// Workaround for the Handoff requirement to deliver an extensionless file.
+    /// https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/Handoff/AdoptingHandoff/AdoptingHandoff.html#//apple_ref/doc/uid/TP40014338-CH2-SW10
+    /// </summary>
+    [RoutePrefix("")]
+    public class ExtensionlessFileDownloadController : Controller
+    {
+        // GET: ExtensionlessFileDownload
+        [Route("apple-app-site-association")]
+        public ActionResult Index()
+        {
+            string filePath = HostingEnvironment.MapPath("~/apple-handoff-json");
+            try
+            {
+                byte[] bytes = System.IO.File.ReadAllBytes(filePath);
+                return File(bytes, "application/pkcs7-mime");
+            }
+            catch
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
