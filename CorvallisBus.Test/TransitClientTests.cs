14277947df35a69d4999bc890469a1446590071c
Rikki Gibson <rikkigibson@gmail.com>
2019-09-15 12:57:01 -0700

    Add TransitClientTests.cs

---
+++ CorvallisBus.Test/TransitClientTests.cs
@@ -0,0 +1,18 @@
+﻿using CorvallisBus.Core.WebClients;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CorvallisBus.Test
+{
+    public class TransitClientTests
+    {
+        [Fact]
+        public void ValidateInitJob()
+        {
+            var client = new TransitClient();
+            _ = client.LoadTransitData();
+        }
+    }
+}
